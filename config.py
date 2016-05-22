39455fd979e17d5a696ca94c0d0b0ff568ccd6ae
alireza <sarsanaee@gmail.com>
2016-05-22 18:03:10 +0430

    Host and port added to config file

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ HMAC_KEY = 'hBV+H7dt2aD/R3z'
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:yokohama@localhost/FarmBase'
 DEBUG = True
 SECRET_KEY = '\xa1xec[\tg`\xac\x96\xafv\xff\xf6\x04\xa2bT\x13\xb6\xca\xf9@\xf2'
+HOST = '0.0.0.0'
+PORT = 6789
--- runserver.py
+++ runserver.py
@@ -1,6 +1,8 @@
 __author__ = 'alireza'
 from Api import app
 
-app.debug = app.config["DEBUG"]
-#manager.run()
-app.run(host='0.0.0.0', port=6789)
+if __name__ == '__main__':
+    app.debug = app.config["DEBUG"]
+    #manager.run()
+    app.run(host=app.config["HOST"], port=app.config["PORT"])
+
