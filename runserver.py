c8f52a25b449d90e6c57dc207dcfa335a1bf0c27
alireza <sarsanaee@gmail.com>
2016-05-28 12:19:17 +0430

    application instead of app

--- runserver.py
+++ runserver.py
@@ -1,12 +1,12 @@
 __author__ = 'alireza'
-from Api import app
+from Api import app as application
 from Api.database import Base
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 
-migrate = Migrate(app, Base)
-manager = Manager(app)
+migrate = Migrate(application, Base)
+manager = Manager(application)
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
