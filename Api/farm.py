84ad24b82c4c9bb2a77749a984b7a93ab206b97d
alireza <sarsanaee@gmail.com>
2016-06-01 18:42:57 +0430

    Daily Threshold Changed

--- Api/farm.py
+++ Api/farm.py
@@ -153,7 +153,7 @@ def daily_reward(id):
     if retrieved_user:
         b = datetime.datetime.now()
         delta = b - retrieved_user.last_daily_reward_date
-        if delta.total_seconds() > 7199:
+        if delta.total_seconds() > app.config["DAILY"]:
             retrieved_user.last_daily_reward_date = datetime.datetime.now()
             db_session.commit()
             response = jsonify({"status": "Ok"})
