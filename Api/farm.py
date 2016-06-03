bab0d58fdbd0e31973072a4b32428fcac80d99bc
alireza <sarsanaee@gmail.com>
2016-06-03 16:43:55 +0430

    not list

--- Api/farm.py
+++ Api/farm.py
@@ -214,10 +214,8 @@ def daily_reward_price(id):
 @app.route('/username/taksos', methods=['POST'])
 def get_username_info():
     retrieved_user = User.query.filter_by(username=request.json["username"]).first()
-    username_list = []
-    username_list.append(retrieved_user.username)
     if retrieved_user:
-        response = jsonify({'username':username_list, 'id': retrieved_user.id, 'password': retrieved_user.password, 'email': retrieved_user.email})
+        response = jsonify({'username':retrieved_user.username, 'id': retrieved_user.id, 'password': retrieved_user.password, 'email': retrieved_user.email})
         response.status_code = 200
         return response
     abort(404)
