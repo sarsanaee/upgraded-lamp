fc285fed1fd3d6951f80a7528416c5e31f5b54bf
Alireza <sarsanaee@gmail.com>
2016-07-12 19:19:00 +0430

    log collector now copy one specific file

--- log_collector.sh
+++ log_collector.sh
@@ -4,5 +4,6 @@ count=`ls -l | grep -v ^l | wc -l`
 file=farm_uwsgi.log.${count}
 cat farm_uwsgi.log > ${file}
 gzip ${file}
-cp * /var/www/data
+cp $file /var/www/data
+rm $file
 truncate -s 0 farm_uwsgi.log
