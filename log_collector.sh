81c5d6714faae4b8d43bf9adf669c6f30b2bf193
Alireza <sarsanaee@gmail.com>
2016-07-12 19:02:41 +0430

    truncate while file is copied completely

--- log_collector.sh
+++ log_collector.sh
@@ -3,6 +3,6 @@ cd /var/log/uwsgi/upgraded-lamp/
 count=`ls -l | grep -v ^l | wc -l`
 file=farm_uwsgi.log.${count}
 cat farm_uwsgi.log > ${file}
-truncate -s 0 farm_uwsgi.log
 gzip ${file}
 cp * /var/www/data
+truncate -s 0 farm_uwsgi.log
