cc246ab8dd449ff1cc3f303cb1a914f51db6a6dc
alireza sanaee <socket@elmogame.dns.parsdev.net>
2016-07-12 19:21:31 +0430

    test

--- log_collector.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /var/log/uwsgi/upgraded-lamp/
-count=`ls -l | grep -v ^l | wc -l`
-file=farm_uwsgi.log.${count}
-cat farm_uwsgi.log > ${file}
-gzip ${file}
-cp * /var/www/data
-truncate -s 0 farm_uwsgi.log
